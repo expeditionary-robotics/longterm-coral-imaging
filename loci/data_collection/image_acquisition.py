6a3ae2fa720c53c39a7916dd8e38e437d555f9c2
vpreston <victorialynnpreston@gmail.com>
2023-11-07 14:18:40 -0500

    creates a utilities file to clean up data collection scripts

--- loci/data_collection/image_acquisition.py
+++ loci/data_collection/image_acquisition.py
@@ -2,105 +2,11 @@
 
 Adapted from VimbaPython/Examples/asynchronous_grab_opencv.py
 """
-import threading
 import os
-import sys
-import cv2
 import argparse
-from typing import Optional
 from vimba import *
 
-
-def abort(reason: str, return_code: int = 1):
-    """Automatic clean abort procedure."""
-    print(reason + '\n')
-    sys.exit(return_code)
-
-
-def get_camera(camera_id: Optional[str]) -> Camera:
-    """Access the target camera."""
-    with Vimba.get_instance() as vimba:
-        if camera_id:
-            try:
-                return vimba.get_camera_by_id(camera_id)
-
-            except VimbaCameraError:
-                abort('Failed to access Camera \'{}\'. Abort.'.format(camera_id))
-
-        else:
-            cams = vimba.get_all_cameras()
-            if not cams:
-                abort('No Cameras accessible. Abort.')
-
-            return cams[0]
-
-
-def setup_camera(cam: Camera):
-    """Set consistent camera settings for image logging.
-    
-    TODO[vpreston] Check that these are the settings we want to use."""
-    with cam:
-        # Enable auto exposure time setting if camera supports it
-        try:
-            cam.ExposureAuto.set('Continuous')
-
-        except (AttributeError, VimbaFeatureError):
-            pass
-
-        # Enable white balancing if camera supports it
-        try:
-            cam.BalanceWhiteAuto.set('Continuous')
-
-        except (AttributeError, VimbaFeatureError):
-            pass
-
-        # Try to adjust GeV packet size. This Feature is only available for GigE - Cameras.
-        try:
-            cam.GVSPAdjustPacketSize.run()
-
-            while not cam.GVSPAdjustPacketSize.is_done():
-                pass
-
-        except (AttributeError, VimbaFeatureError):
-            pass
-
-        # Query available, open_cv compatible pixel formats
-        # prefer color formats over monochrome formats
-        cv_fmts = intersect_pixel_formats(cam.get_pixel_formats(), OPENCV_PIXEL_FORMATS)
-        color_fmts = intersect_pixel_formats(cv_fmts, COLOR_PIXEL_FORMATS)
-
-        if color_fmts:
-            cam.set_pixel_format(color_fmts[0])
-
-        else:
-            mono_fmts = intersect_pixel_formats(cv_fmts, MONO_PIXEL_FORMATS)
-
-            if mono_fmts:
-                cam.set_pixel_format(mono_fmts[0])
-
-            else:
-                abort('Camera does not support a OpenCV compatible format natively. Abort.')
-
-
-class FrameHandler:
-    def __init__(self):
-        self.shutdown_event = threading.Event()
-
-    def __call__(self, cam: Camera, frame: Frame):
-        ENTER_KEY_CODE = 13
-
-        key = cv2.waitKey(1)
-        if key == ENTER_KEY_CODE:
-            self.shutdown_event.set()
-            return
-
-        elif frame.get_status() == FrameStatus.Complete:
-            print('{} acquired {}'.format(cam, frame), flush=True)
-
-            msg = 'Stream from \'{}\'. Press <Enter> to stop stream.'
-            cv2.imshow(msg.format(cam.get_name()), frame.as_opencv_image())
-
-        cam.queue_frame(frame)
+from loci.data_collection.utils import get_camera, setup_camera, FrameHandler
 
 
 if __name__ == '__main__':
@@ -118,4 +24,3 @@ if __name__ == '__main__':
 
             finally:
                 cam.stop_streaming()
-
